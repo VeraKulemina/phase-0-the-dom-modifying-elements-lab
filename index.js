c1560954e0e9efef998fff477f0640a4493c8d35
VeraKulemina <verakulemina@gmail.com>
2022-09-24 12:22:30 -0600

    commit tree d070fd6b6cd4d9c630aba9ea713d859e7b41cb11

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+//document.body.append(newHeader);
+
+newHeader.id = "victory";
+
+newHeader.textContent = 'Vera is the champion';
+
+document.body.append(newHeader);
